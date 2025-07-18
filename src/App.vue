ac231a8844a20b7cae40759777c882f18fa56587
Gloumy <simon@cornet.dev>
2025-07-18 15:09:53 +0200

    dialog pour les details du club

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,11 @@
 <script setup lang="ts">
-import Map from './components/Map.vue'
+import Map from "./components/Map.vue";
 </script>
 
 <template>
   <v-app>
     <v-main>
-      <v-app-bar flat="" title="Adopte un sport"></v-app-bar>
+      <v-app-bar flat title="Adopte un sport"></v-app-bar>
       <Map />
     </v-main>
   </v-app>
--- src/components/Map.vue
+++ src/components/Map.vue
@@ -5,7 +5,6 @@
       v-model:zoom="zoom"
       :use-global-leaflet="false"
       :center="[49.5724501273382, 5.535054262725816]"
-
     >
       <l-tile-layer
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
@@ -13,10 +12,23 @@
         name="OpenStreetMap"
         attribution="&copy; OpenStreetMap contributors"
       />
-      <l-marker v-for="club in clubs" :key="club.nom" :lat-lng="[club.latitude, club.longitude]" :name="club.nom">
-        <l-popup>{{ club.nom }}</l-popup>
+      <l-marker
+        v-for="club in clubs"
+        :key="club.nom"
+        :lat-lng="[club.latitude, club.longitude]"
+        :name="club.nom"
+      >
+        <l-popup>
+          <span>{{ club.nom }}</span>
+          <v-icon @click="openDetailsDialog(club)">mdi-open-in-new</v-icon>
+        </l-popup>
       </l-marker>
     </l-map>
+    <v-dialog v-model="displayDetailsDialog">
+      <v-card>
+        <v-card-title>{{ selectedClub?.nom }}</v-card-title>
+      </v-card>
+    </v-dialog>
   </div>
 </template>
 
@@ -26,18 +38,25 @@ import "leaflet/dist/leaflet.css";
 import { LMap, LTileLayer, LMarker, LPopup } from "@vue-leaflet/vue-leaflet";
 // import type L from "leaflet";
 import useClubApi from "../api/ClubApi";
-import type { Club } from "../models/Club";
+import { Club } from "../models/Club";
 
 const { getClubs } = useClubApi();
 
 // Variables réactives
 const zoom = ref(15);
+const displayDetailsDialog = ref(false);
+
 const clubs = ref<Club[]>([]);
+const selectedClub = ref<Club>();
 
-onMounted(async() => {
+onMounted(async () => {
   clubs.value = await getClubs();
-})
+});
 
+function openDetailsDialog(club: Club) {
+  selectedClub.value = new Club(club);
+  displayDetailsDialog.value = true;
+}
 </script>
 
 <style scoped>
